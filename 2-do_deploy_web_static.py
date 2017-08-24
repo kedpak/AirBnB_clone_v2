02702dd2b33ff63ab596c0af2c42166ef41efb89
kedpak <kedpak@yahoo.com>
2017-08-24 21:02:30 +0000

    change syntax

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,25 +12,25 @@ from fabric.operations import run, put, sudo
 
 env.hosts = ['66.70.184.29', '34.228.245.175']
 
-def do_deploy(archive_path):
 
+def do_deploy(archive_path):
+    """that creates and distributes an archive to web servers"""
 
     if os.path.isfile(archive_path) is False:
         return False
+
     try:
         archive = archive_path.split('/')[-1]
         folder = archive.split('.')[0]
-        print('before put')
+        strng = "data/web_static/releases"
         put(archive_path, "/tmp/")
-        print('after put')
         run("sudo mkdir -p /data/web_static/releases/{}/".format(folder))
-        run("sudo tar -xzf /tmp/ {} -C /data/web_static/releases/{}/".format(archive, folder))
+        run("sudo tar -xzf /tmp/{} -C /{}/{}/".format(archive, strng, folder))
         run("sudo rm /tmp/{}".format(archive))
-        run("sudo mv /data/web_static/releases/{}/* /data/web_static/releases/{}/".format(folder, folder))
-        run("sudo rm -rf /data/web_static/releases{}/web_static".format(folder))
+        run("sudo mv /{0}/{1}/web_static/* /{0}/{1}/".format(strng, folder))
+        run("sudo rm -rf /{}/{}/web_static".format(strng, folder))
         run("sudo rm -rf /data/web_static/current")
-        run("sudo ln -s /data/web_static/releases/{} /data/web_static/current".format(folder))
+        run("sudo ln -s /{}/{} /data/web_static/current".format(strng, folder))
         return True
     except Exception as e:
-        print(e)
         return False
