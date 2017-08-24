c96d96b3f1e793ec9a86e7d271db6d1c5d184cc5
kedpak <kedpak@yahoo.com>
2017-08-24 21:36:11 +0000

    first commit

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,55 @@
+#!/usr/bin/pythhon3
+"""
+distribute an archive to web servers
+"""
+import os
+import os.path
+from time import strftime
+from datetime import datetime
+from fabric.api import *
+from fabric.operations import run, put, sudo
+
+env.hosts = ['66.70.184.29', '34.228.245.175']
+
+def do_pack():
+    ''' pack web_static directory into tgz archive
+    '''
+    try:
+        time = datetime.utcnow().strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static".format(time))
+        return("versions/web_static_{}.tgz".format(time))
+    except:
+        return(None)
+
+
+def do_deploy(archive_path):
+    """that creates and distributes an archive to web servers"""
+
+    if os.path.isfile(archive_path) is False:
+        return False
+
+    try:
+        archive = archive_path.split('/')[-1]
+        folder = archive.split('.')[0]
+        strng = "data/web_static/releases"
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p /data/web_static/releases/{}/".format(folder))
+        run("sudo tar -xzf /tmp/{} -C /{}/{}/".format(archive, strng, folder))
+        run("sudo rm /tmp/{}".format(archive))
+        run("sudo mv /{0}/{1}/web_static/* /{0}/{1}/".format(strng, folder))
+        run("sudo rm -rf /{}/{}/web_static".format(strng, folder))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s /{}/{} /data/web_static/current".format(strng, folder))
+        return True
+    except Exception as e:
+        return False
+
+def deploy():
+    """creates and distributes an archive to web servers"""
+    try:
+        pack = do_pack()
+        d_deploy = do_deploy(archive_path)
+        return(d_deploy)
+    except:
+        return(False)
