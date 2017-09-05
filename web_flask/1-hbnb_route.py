3193a8ef20814cfa9c46980043828af6d33f6f8a
kedpak <kedpak@yahoo.com>
2017-09-05 23:46:30 +0000

    first commit

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that starts a Flask web application.
+/: display "Hello HBNB!"
+/hbnb: display "HBNB"
+"""
+
+from flask import Flask
+from flask import request
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/', methods=['GET'])
+def hello_route():
+    return ("Hello HBNB!")
+
+@app.route('/hbnb', methods=['GET'])
+def hbnb_route():
+    return ("HBNB")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
